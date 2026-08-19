ebb93757487e7d5a3d3faf50da552588010f3f31
rizkialil <rizkyalmurtadlo@gmail.com>
2026-08-19 23:56:13 +0700

    Update LoginView.tsx

--- src/components/LoginView.tsx
+++ src/components/LoginView.tsx
@@ -56,7 +56,7 @@ export default function LoginView({ onLoginSuccess, config, isDark }: LoginViewP
             <div className="relative z-10">
               <div className="flex items-center gap-3.5 mb-8 sm:mb-12">
                 {/* Person Icon in Circle */}
-                <div className="inline-flex p-3.5 bg-white/10 border border-white/10 rounded-full w-fit shadow-inner">
+                <div className="inline-flex p-1.5 bg-white/10 border border-white/10 rounded-full w-fit shadow-inner">
                   <img
                     src="/logoweb.png"
                     alt="Logo"
